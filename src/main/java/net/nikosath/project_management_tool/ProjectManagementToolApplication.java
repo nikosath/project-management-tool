c5811eb39d342584c2b6863566a1028282a4f723
nathanasakis <emidekol@gmail.com>
2020-06-09 00:52:15 +0300

    First commit

---
+++ src/main/java/net/nikosath/project_management_tool/ProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package net.nikosath.project_management_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/nikosath/project_management_tool/ProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nikosath.project_management_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
